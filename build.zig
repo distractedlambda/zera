fb9297dae60fdf630ba3c1b5721cb76ecb340633
Lucas Myers <lucascharlesmyers@gmail.com>
2023-05-29 10:08:52 -0400

    WIP

--- build.zig
+++ build.zig
@@ -26,78 +26,90 @@ const wasm_wasi_target = std.zig.CrossTarget{
     .os_tag = .wasi,
 };
 
-pub fn build(b: *std.Build) void {
-    // Standard target options allows the person running `zig build` to choose
-    // what target to build for. Here we do not override the defaults, which
-    // means any target is allowed, and the default is native. Other options
-    // for restricting supported target set are available.
-    const target = b.standardTargetOptions(.{});
+const Configurator = struct {
+    build: *std.Build,
+    target: ?std.zig.CrossTarget = null,
+    optimize_mode: ?std.builtin.OptimizeMode = null,
+    run_unit_tests: ?*std.Build.Step.Run = null,
 
-    // Standard optimization options allow the person running `zig build` to select
-    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall. Here we do not
-    // set a preferred release mode, allowing the user to decide how to optimize.
-    const optimize = b.standardOptimizeOption(.{});
+    fn addWasmModule(
+        self: *@This(),
+        name: []const u8,
+        root_source_file: std.Build.FileSource,
+        target: std.zig.CrossTarget,
+        optimize: std.builtin.OptimizeMode,
+    ) *std.Build.Step.Compile {
+        const module = self.build.addSharedLibrary(.{
+            .name = name,
+            .root_source_file = root_source_file,
+            .target = target,
+            .optimize = optimize,
+        });
 
-    const exe = b.addExecutable(.{
-        .name = "zera",
-        // In this case the main source file is merely a path, however, in more
-        // complicated build scripts, this could be a generated file.
-        .root_source_file = .{ .path = "src/main.zig" },
-        .target = target,
-        .optimize = optimize,
-    });
+        module.rdynamic = true;
 
-    const validate_utf8_module = b.addSharedLibrary(.{
-        .name = "validate_utf8",
-        .root_source_file = .{ .path = "src/test_modules/validate_utf8.zig" },
-        .target = wasm_freestanding_target,
-        .optimize = .ReleaseSmall,
-    });
-
-    const install_validate_utf8 = b.addInstallArtifact(validate_utf8_module);
-
-    // This declares intent for the executable to be installed into the
-    // standard location when the user invokes the "install" step (the default
-    // step when running `zig build`).
-    b.installArtifact(exe);
-
-    // This *creates* a Run step in the build graph, to be executed when another
-    // step is evaluated that depends on it. The next line below will establish
-    // such a dependency.
-    const run_cmd = b.addRunArtifact(exe);
-
-    // By making the run step depend on the install step, it will be run from the
-    // installation directory rather than directly from within the cache directory.
-    // This is not necessary, however, if the application depends on other installed
-    // files, this ensures they will be present and in the expected location.
-    run_cmd.step.dependOn(b.getInstallStep());
-
-    // This allows the user to pass arguments to the application in the build
-    // command itself, like this: `zig build run -- arg1 arg2 etc`
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
+        return module;
     }
 
-    // This creates a build step. It will be visible in the `zig build --help` menu,
-    // and can be selected like this: `zig build run`
-    // This will evaluate the `run` step rather than the default, which is "install".
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_cmd.step);
+    fn addFreestandingWasmModule(
+        self: *@This(),
+        name: []const u8,
+        root_source_file: std.Build.FileSource,
+        optimize: std.builtin.OptimizeMode,
+    ) *std.Build.Step.Compile {
+        return self.addWasmModule(name, root_source_file, wasm_freestanding_target, optimize);
+    }
 
-    // Creates a step for unit testing. This only builds the test executable
-    // but does not run it.
-    const unit_tests = b.addTest(.{
-        .root_source_file = .{ .path = "src/main.zig" },
-        .target = target,
-        .optimize = optimize,
-    });
+    fn addWasiModule(
+        self: *@This(),
+        name: []const u8,
+        root_source_file: std.Build.FileSource,
+        optimize: std.builtin.OptimizeMode,
+    ) *std.Build.Step.Compile {
+        return self.addWasmModule(name, root_source_file, wasm_wasi_target, optimize);
+    }
 
-    const run_unit_tests = b.addRunArtifact(unit_tests);
-    run_unit_tests.step.dependOn(&install_validate_utf8.step);
+    fn configureTestWasmModule(self: *@This(), name: []const u8) void {
+        const root = std.build.FileSource{ .path = self.build.fmt("src/test_modules/{s}.zig", .{name}) };
+        for (std.meta.tags(std.builtin.OptimizeMode)) |mode| {
+            const mode_specific_name = self.build.fmt("{s}-{s}", .{name, @tagName(mode)});
+            const module = self.addFreestandingWasmModule(mode_specific_name, root, mode);
+            const install_module = self.build.addInstallArtifact(module);
+            install_module.dest_dir = .{ .custom = "test_modules" };
+            self.run_unit_tests.?.step.dependOn(&install_module.step);
+        }
+    }
 
-    // Similar to creating the run step earlier, this exposes a `test` step to
-    // the `zig build --help` menu, providing a way for the user to request
-    // running the unit tests.
-    const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_unit_tests.step);
+    fn configureAllTestWasmModules(self: *@This()) void {
+        for (@import("src/test_modules/manifest.zig").module_names) |name| {
+            self.configureTestWasmModule(name);
+        }
+    }
+
+    fn configureUnitTests(self: *@This()) void {
+        const unit_tests = self.build.addTest(.{
+            .root_source_file = .{ .path = "src/main.zig" },
+            .target = self.target.?,
+            .optimize = self.optimize_mode.?,
+        });
+
+        self.run_unit_tests = self.build.addRunArtifact(unit_tests);
+        self.run_unit_tests.?.cwd = self.build.install_path;
+
+        self.configureAllTestWasmModules();
+
+        const test_step = self.build.step("test", "Run unit tests");
+        test_step.dependOn(&self.run_unit_tests.?.step);
+    }
+
+    fn configureBuild(self: *@This()) void {
+        self.target = self.build.standardTargetOptions(.{});
+        self.optimize_mode = self.build.standardOptimizeOption(.{});
+        self.configureUnitTests();
+    }
+};
+
+pub fn build(b: *std.Build) void {
+    var configurator = Configurator{ .build = b };
+    configurator.configureBuild();
 }
---
+++ src/test_modules/manifest.zig
@@ -0,0 +1,3 @@
+pub const module_names = [_][]const u8{
+    "validate_utf8",
+};
